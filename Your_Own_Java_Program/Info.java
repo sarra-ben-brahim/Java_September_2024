bde3c8e3020fec71a508d88eed323b81578b53c2
Sarra Ben Brahim <sarra-ben-brahim@users.noreply.github.com>
2024-09-27 19:11:32 +0100

    Your own java program commit

---
+++ Your_Own_Java_Program/Info.java
@@ -0,0 +1,9 @@
+public class Info {
+	public static void main(String[] args) {
+        
+        System.out.println("My name is Sarra");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is Medenine, Tunisia");
+
+	}
+}

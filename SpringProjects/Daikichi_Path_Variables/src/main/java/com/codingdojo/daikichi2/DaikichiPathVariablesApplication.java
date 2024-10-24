5c0b21ada26a50b0edb0a8fa6534285b9dc05a41
Sarra Ben Brahim <sarra-ben-brahim@users.noreply.github.com>
2024-10-24 11:07:04 +0100

    Daikichi Path Variables core assignement

---
+++ SpringProjects/Daikichi_Path_Variables/src/main/java/com/codingdojo/daikichi2/DaikichiPathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+	}
+
+}
---
+++ SpringProjects/Daikichi_Path_Variables/src/test/java/com/codingdojo/daikichi2/DaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

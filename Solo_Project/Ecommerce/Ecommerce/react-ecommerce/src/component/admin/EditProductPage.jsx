81c075698997354b1c84d52e3c89e374f02c6160
Sarra Ben Brahim <sarra-ben-brahim@users.noreply.github.com>
2024-11-29 16:22:33 +0100

    front end commit

---
+++ Solo_Project/Ecommerce/Ecommerce/react-ecommerce/src/component/admin/EditProductPage.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import '../../style/addProduct.css'
+import ApiService from "../../service/ApiService";
+
+
+const EditProductPage = () => {
+    const { productId } = useParams();
+    const [image, setImage] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [categoryId, setCategoryId] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [message, setMessage] = useState('');
+    const [price, setPrice] = useState('');
+    const [isImageVisible, setIsImageVisible] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        ApiService.getAllCategory().then((res) => setCategories(res.categoryList));
+
+        if (productId) {
+            ApiService.getProductById(productId).then((response) => {
+                setName(response.product.name);
+                setDescription(response.product.description);
+                setPrice(response.product.price);
+                setCategoryId(response.product.categoryId);
+                setImage(response.product.image);
+            })
+        }
+    }, [productId]);
+
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+        setIsImageVisible(false);
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = new FormData();
+            if (image) {
+                formData.append('image', image);
+            }
+            formData.append('productId', productId);
+            formData.append('categoryId', categoryId);
+            formData.append('name', name);
+            formData.append('description', description);
+            formData.append('price', price);
+
+            const response = await ApiService.updateProduct(formData);
+            if (response.status === 200) {
+                setMessage(response.message)
+                setTimeout(() => {
+                    setMessage('')
+                    navigate('/admin/products')
+                }, 1000);
+            }
+
+        } catch (error) {
+            setMessage(error.response?.data?.message || error.message || 'unable to update product')
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="product-form">
+            <h2>Edit Product</h2>
+            {message && <div className="message">{message}</div>}
+            <input type="file" onChange={handleImageChange} />
+            {isImageVisible && (
+                <img alt={name} src={"data:image/jpeg;base64," + image} className="product-image" />
+            )}
+
+            <select value={categoryId} onChange={(e) => setCategoryId(e.target.value)}>
+                <option value="">Select Category</option>
+                {categories.map((cat) => (
+                    <option value={cat.id} key={cat.id}>{cat.name}</option>
+                ))}
+            </select>
+
+            <input type="text"
+                placeholder="Product name"
+                value={name}
+                onChange={(e) => setName(e.target.value)} />
+
+            <textarea
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)} />
+
+            <input type="number"
+                placeholder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)} />
+
+            <button type="submit">Update</button>
+        </form>
+    );
+}
+
+export default EditProductPage;
